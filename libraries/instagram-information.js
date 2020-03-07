1637864e8ed2ee50e890b22a0f63861a617eb4d0
Attila BADI <attila.badi@gmail.com>
2020-03-07 11:24:23 +0000

    Instagram integration step 2

--- libraries/instagram-information.js
+++ libraries/instagram-information.js
@@ -1,51 +1,30 @@
-
-
-function loadPicturesHTML(repos) {
-    if (repos.length == 0) {
-        return `<div class="clearfix repo-list">No repos!</div>`;
-    }
-
-    var listItemsHTML = repos.map(function(repo) {
-        return `<li>
-                    <a href="${repo.media_url}" target="_blank">${repo.media_url}</a>
-                </li>`;
-    });
-
-    return `<div class="clearfix repo-list">
-                <p>
-                    <strong>Repo List:</strong>
-                </p>
-                <ul>
-                    ${listItemsHTML.join("\n")}
-                </ul>
-            </div>`;
-}
-
 function fetchInstagramInformation(event) {
     $("#instagram-pictures").html("");
-    
-    $("#instagram-pictures").html(
-        `<div id="loader"><p>Loading</p>
-        </div>`);
+    $("#instagram-pictures").html(`<div id="loader"><p>Loading</p></div>`);
+
+    //        $.getJSON("https://graph.instagram.com/me/media?fields=caption,id,media_type,media_url&access_token=IGQVJWRURFRl9ROUJmNzgyX21yQ3lESnZAQeklILVRFOGpVUXBVY3g0aFFKZAlVFVHZA2WFZARU05uMXgtRFE0dTFCbUhFamdjWWJuZAmNCM1VZAZAmY5VGdIejdDSEVTaVlDTHNYSlY4UGZASLXREQW9SZA0VRYwZDZD")
 
     $.when(
-        $.getJSON(`https://graph.instagram.com/me/media?fields=caption,id,media_type,media_url&access_token=IGQVJWRURFRl9ROUJmNzgyX21yQ3lESnZAQeklILVRFOGpVUXBVY3g0aFFKZAlVFVHZA2WFZARU05uMXgtRFE0dTFCbUhFamdjWWJuZAmNCM1VZAZAmY5VGdIejdDSEVTaVlDTHNYSlY4UGZASLXREQW9SZA0VRYwZDZD`),
+        $.getJSON(`https://api.github.com/users/octocat`)
     ).then(
         function(firstResponse) {
             var userData = firstResponse[0];
-            $("#instagram-pictures").html(loadPicturesHTML(userData));
+            $("#instagram-pictures").html(`<p>loaded</p>`);
         },
         function(errorResponse) {
             if (errorResponse.status === 404) {
-                $("#instagram-user-data").html(
-                    `<h2>No info found</h2>`);
+                $("#instagram-pictures").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            }
+            else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#instagram-pictures").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             }
             else {
                 console.log(errorResponse);
-                $("#instagram-user-data").html(
+                $("#instagram-pictures").html(
                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
         });
-}
 
-$(document).ready(fetchInstagramInformation);
+}
